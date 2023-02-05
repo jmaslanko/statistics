e616f1e6b99fc0d377512105756aab9fb69a1075
Jeremy Maslanko <maslankoj@gmail.com>
2023-02-04 21:42:56 -0500

    add structure fxn

--- Reliability.py
+++ Reliability.py
@@ -1,4 +1,11 @@
 
 
 def structure_function(x1, x2, x3, x4):
+    '''
+    This formula unfortunately is not very dynamic.
+    
+    However, once you edit for the specific structure 
+    function you are working with, a great way to verify
+    block diagrams are drawn correctly.
+    '''
     return 1-(1-x1)*(1-x2*(1-(1-x3)*(1-x4)))
